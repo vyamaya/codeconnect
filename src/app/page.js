a6a16331b943f77714a470858e53457a712d8da2
vinicius.yamaya <vyamaya@hotmail.com>
2025-01-03 16:06:18 -0300

    add seed

--- src/app/page.js
+++ src/app/page.js
@@ -2,35 +2,18 @@ import { CardPost } from "@/components/CardPost";
 import logger from "@/logger";
 import styles from "./page.module.css";
 import Link from "next/link";
-
-// const post = {
-//   id: 1,
-//   cover:
-//     "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/posts/introducao-ao-react.png",
-//   title: "Introdução ao React",
-//   slug: "introducao-ao-react",
-//   body: "Neste post, vamos explorar os conceitos básicos do React, uma biblioteca JavaScript para construir interfaces de usuário. Vamos cobrir componentes, JSX e estados.",
-//   markdown:
-//     "```javascript\nfunction HelloComponent() {\n  return <h1>Hello, world!</h1>;\n}\n```",
-//   author: {
-//     id: 101,
-//     name: "Ana Beatriz",
-//     username: "anabeatriz_dev",
-//     avatar:
-//       "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/authors/anabeatriz_dev.png",
-//   },
-// };
+import db from "../../prisma/db";
 
 async function getAllPosts(page) {
-  const response = await fetch(
-    `http://localhost:3042/posts?_page=${page}&_per_page=6`
-  );
-  if (!response.ok) {
-    logger.error("Ops, alguma coisa ocorreu ma!l");
-    return [];
+  try {
+    
+    const posts = await db.post.findMany()
+
+    return { data: posts, prev: null, next: null }
+  } catch (error) {
+    logger.error('falha ao obter posts', {error})
+    return { data: [], prev: null, next: null }
   }
-  logger.info("Posts obtidos com sucesso!");
-  return response.json();
 }
 
 export default async function Home({ searchParams }) {
