20c4702072e42c13ac28bde9ab170cfb3e68b972
vinicius.yamaya <vyamaya@hotmail.com>
2025-01-03 17:35:51 -0300

    feat: add not-found page e formulario de busca

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Prompt } from "next/font/google";
 
 import { Aside } from "@/components/Aside";
 import "./globals.css";
+import { SearchForm } from "@/components/SearchForm";
 
 export const metadata = {
   title: "Code Connect",
@@ -22,7 +23,10 @@ export default function RootLayout({ children }) {
           <div>
             <Aside />
           </div>
-          <div className="main-content">{children}</div>
+          <div className="main-content">
+            <SearchForm />
+            {children}
+          </div>
         </div>
       </body>
     </html>
--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -4,24 +4,33 @@ import html from "remark-html";
 import styles from "./page.module.css";
 import { CardPost } from "@/components/CardPost";
 import db from "../../../../prisma/db";
+import { redirect } from "next/navigation";
 
 async function getPostBySlug(slug) {
+  try {
+    const post = await db.post.findFirst({
+      where: {
+        slug,
+      },
+      include: {
+        author: true,
+      },
+    });
 
-  const post = await db.post.findFirst({
-    where: {
-      slug
-    },
-    include:{
-      author: true
+    if(!post){
+      throw new Error(`Post com o slug ${slug} não foi encontrado`)
     }
-  })
 
-  const processedContent = await remark().use(html).process(post.markdown);
-  const contentHtml = processedContent.toString();
+    const processedContent = await remark().use(html).process(post.markdown);
+    const contentHtml = processedContent.toString();
 
-  post.markdown = contentHtml;
+    post.markdown = contentHtml;
 
-  return post;
+    return post;
+  } catch (error) {
+    logger.error("Falha ao obter o post com o slug: ", { slug, error });
+  }
+  redirect('/not-found')
 }
 
 const PagePost = async ({ params }) => {
