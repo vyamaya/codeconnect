e65bf88a3da558aaead9b04ea9fce15c642e945a
vinicius.yamaya <vyamaya@hotmail.com>
2025-01-03 16:21:04 -0300

    estilizado pagina de erro

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,11 @@ import db from "../../prisma/db";
 async function getAllPosts(page) {
   try {
     
-    const posts = await db.post.findMany()
+    const posts = await db.post.findMany({
+      include: {
+        author: true
+      }
+    })
 
     return { data: posts, prev: null, next: null }
   } catch (error) {
