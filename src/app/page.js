fb737a7f47079af576f33e22cefd80050f0c0bd7
vinicius.yamaya <vyamaya@hotmail.com>
2025-01-06 17:00:40 -0300

    add busca

--- src/app/page.js
+++ src/app/page.js
@@ -4,19 +4,29 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import db from "../../prisma/db";
 
-async function getAllPosts(page) {
+async function getAllPosts(page, searchTerm) {
   try {
+    const where = {};
+
+    if (searchTerm) {
+      where.title = {
+        contains: searchTerm,
+        mode: "insensitive",
+      };
+    }
+
     const perPage = 6;
-    const skip = (page - 1) * perPage
-    const totalItems = await db.post.count()
-    const totalPages = Math.ceil(totalItems / perPage)
-    const prev = page > 1 ? page -1 : null
-    const next = page < totalPages ? page + 1 : null
+    const skip = (page - 1) * perPage;
+    const totalItems = await db.post.count({ where });
+    const totalPages = Math.ceil(totalItems / perPage);
+    const prev = page > 1 ? page - 1 : null;
+    const next = page < totalPages ? page + 1 : null;
 
     const posts = await db.post.findMany({
       take: perPage,
       skip,
-      orderBy: {createdAt: 'desc'},
+      where,
+      orderBy: { createdAt: "desc" },
       include: {
         author: true,
       },
@@ -30,16 +40,21 @@ async function getAllPosts(page) {
 }
 
 export default async function Home({ searchParams }) {
-  const currentPage = parseInt(searchParams?.page || 1)
-  const { data: posts, prev, next } = await getAllPosts(currentPage);
+  const currentPage = parseInt(searchParams?.page || 1);
+  const searchTerm = searchParams?.q;
+  const {
+    data: posts,
+    prev,
+    next,
+  } = await getAllPosts(currentPage, searchTerm);
   return (
     <main className={styles.grid}>
       {posts.map((post) => (
         <CardPost key={post.id} post={post} />
       ))}
       <div className={styles.links}>
-        {prev && <Link href={`/?page=${prev}`}>Página anterior</Link>}
-        {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
+        {prev && <Link href={{ pathname: "/", query: {page: prev, q: searchTerm} }}>Página anterior</Link>}
+        {next && <Link href={{ pathname: "/", query: {page: next, q: searchTerm} }}>Próxima página</Link>}
       </div>
     </main>
   );
